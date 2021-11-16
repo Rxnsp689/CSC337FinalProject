f603a13edab9dd4891374f0ae68c19ab2692d242
Sophia Wang <43829066+Rxnsp689@users.noreply.github.com>
2021-11-16 15:05:55 -0700

    Schemas and Routes added
    The basic functionality and structure of the backend is done. Need to add validation

---
+++ schemas/canvas.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const CanvasSchema = new Schema({
+    user_id: {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    },
+
+    data_url: {
+        type: String,
+    }
+
+})
+
+Canvas = mongoose.model('Canvas', CanvasSchema);
+module.exports = Canvas
