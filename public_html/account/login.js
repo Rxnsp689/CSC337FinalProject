77896fe7fdc46e593060f7935735d3ae00b7a793
dryng <ryngler.daniel@gmail.com>
2021-11-20 21:41:31 -0700

    basic front end w/o drawing capabilites

---
+++ public_html/account/login.js
@@ -0,0 +1,28 @@
+/* Log user in */
+function login() {
+    let u = $('#usernameLogin').val();
+    let p = $('#passwordLogin').val();
+    $('#usernameLogin').val('');
+    $('#passwordLogin').val('');
+    $.get(
+      '/account/login/' + u + '/' + encodeURIComponent(p),
+      (data, status) => {
+          alert(data);
+          if (data == 'LOGIN') {
+            window.location.href = '/app/home.html';
+          }
+    });
+}
+/* Create a new account */  
+function createAccount() {
+    let u = $('#usernameCreate').val();
+    let p = $('#passwordCreate').val();
+    $('#usernameCreate').val('');
+    $('#passwordCreate').val('');
+    $.get(
+      '/account/create/' + u + '/' + encodeURIComponent(p),
+      (data, status) => {
+          alert(data);
+    });
+}
+  
